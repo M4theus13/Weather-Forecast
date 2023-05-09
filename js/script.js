bc789226715528afdded2ca2cea468f042ba3888
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-05-08 23:31:12 -0300

    finalized the project and api call

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@ const key = '63bd84f73c0b0036ac56402d126fccdb';
 
 function dataScreen(data) {
     document.querySelector('.city').innerHTML = 'Tempo em ' + data.name
+    document.querySelector('.temp').innerHTML = Math.floor(data.main.temp) + '°C'
+    document.querySelector('.weather-text').innerHTML = data.weather[0].description
+    document.querySelector('.humidity').innerHTML = 'Umidade: ' + data.main.humidity + '%'
+    document.querySelector('.weather-image').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
     console.log(data)
 }
 
 async function searchCity(city) {
 
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`).then(result => result.json())
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(result => result.json())
 
     dataScreen(data)
 }
