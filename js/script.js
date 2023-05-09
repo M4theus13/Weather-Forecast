d401c24fed2a9593302b18205d6d09b132d7daf6
Matheus Garcia <matheusgarciajbs@gmail.com>
2023-05-09 10:28:26 -0300

    geolocation implemented and code improvements

--- js/script.js
+++ js/script.js
@@ -1,22 +1,46 @@
 const key = '63bd84f73c0b0036ac56402d126fccdb';
 
+async function getPosition(position) {
+    const dados = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    }
+    const init = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${dados.latitude}&lon=${dados.longitude}&appid=${key}&lang=pt_br&units=metric`).then(result => result.json())
+    dataInit(init)
+}
+
+function dataInit(init) {
+    document.querySelector('.city').innerHTML = 'Tempo em ' + init.name;
+    document.querySelector('.temp').innerHTML = Math.floor(init.main.temp) + '°C';
+    document.querySelector('.weather-text').innerHTML = init.weather[0].description;
+    document.querySelector('.humidity').innerHTML = 'Umidade: ' + init.main.humidity + '%';
+    document.querySelector('.weather-image').src = `https://openweathermap.org/img/wn/${init.weather[0].icon}.png`;
+}
+
+function geoError(error) {
+    if (error.code == 1){
+        alert('O usuário não permimtiu a geolocalização')
+    }
+}
+
+navigator.geolocation.getCurrentPosition(getPosition, geoError)
+
 function dataScreen(data) {
-    document.querySelector('.city').innerHTML = 'Tempo em ' + data.name
-    document.querySelector('.temp').innerHTML = Math.floor(data.main.temp) + '°C'
-    document.querySelector('.weather-text').innerHTML = data.weather[0].description
-    document.querySelector('.humidity').innerHTML = 'Umidade: ' + data.main.humidity + '%'
-    document.querySelector('.weather-image').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    document.querySelector('.city').innerHTML = 'Tempo em ' + data.name;
+    document.querySelector('.temp').innerHTML = Math.floor(data.main.temp) + '°C';
+    document.querySelector('.weather-text').innerHTML = data.weather[0].description;
+    document.querySelector('.humidity').innerHTML = 'Umidade: ' + data.main.humidity + '%';
+    document.querySelector('.weather-image').src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
 }
 
 async function searchCity(city) {
 
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(result => result.json())
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(result => result.json());
 
-    dataScreen(data)
+    dataScreen(data);
 }
 
 function clickButton() {
-    let city = document.querySelector('.input-city').value;
-
-    searchCity(city)
+    let city = document.querySelector('.input-city').value.trimEnd();
+    searchCity(city);
 }
